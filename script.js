dde758895476184dab8aee7d6e3128ac641bbfee
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-28 16:13:00 -0600

    saves inputs on loss of focus in text area

---
+++ script.js
@@ -0,0 +1,71 @@
+$('saveBtn').on('click', function(event){
+    event.preventDefault();
+})
+
+$('input').focusout(function(){
+    console.log("no focus");
+    var eight = {
+        text: $('#eight').val()
+    }
+    console.log(eight);
+})
+$('#nine').focusout(function(){
+    var nine = {
+        text: $('#nine').val()
+    }
+    console.log(nine);
+})
+$('#ten').focusout(function(){
+    var ten = {
+        text: $('#ten').val()
+    }
+    console.log(ten);
+})
+$('#eleven').focusout(function(){
+    var eleven = {
+        text: $('#eleven').val()
+    }
+    console.log(eleven);
+})
+$('#twelve').focusout(function(){
+    var twelve = {
+        text: $('#twelve').val()
+    }
+    console.log(twelve);
+})
+$('#one').focusout(function(){
+    var one = {
+        text: $('#one').val()
+    }
+    console.log(one);
+})
+$('#two').focusout(function(){
+    var two = {
+        text: $('#two').val()
+    }
+    console.log(two);
+})
+$('#three').focusout(function(){
+    var three = {
+        text: $('#three').val()
+    }
+    console.log(three);
+})
+$('#four').focusout(function(){
+    var four = {
+        text: $('#four').val()
+    }
+    console.log(four);
+})
+$('#five').focusout(function(){
+    var five = {
+        text: $('#five').val()
+    }
+    console.log(five);
+})
+$('#six').focusout(function(){
+    var six = {
+        text: $('#six').val()
+    }
+    console.log(six);
+})
